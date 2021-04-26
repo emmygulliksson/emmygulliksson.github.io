c0dadfacd40f9d475bda89e0979a1f71adc4f465
emmygulliksson <id18egn@cs.umu.se>
2021-04-26 12:28:01 +0200

    hej

---
+++ JAVASCRIPT/javascript.js
@@ -0,0 +1,35 @@
+
+$(window).on("scroll", function() {
+   if ($(this).scrollTop() > 10) {
+     $("header").css("background","rgb(233, 234, 236, 0.9)");
+      
+   }
+   else {
+      $("header").css("background","none");
+   }
+});
+
+
+
+$(document).ready(function(){
+    $(".box-img-left").mouseenter(function() {
+        $('.inner-box-left').stop().fadeIn(300);
+        $(".inner-box-left").css("display","grid");
+    });
+    
+    $(".box-img-left, .inner-box-left").mouseleave(function() {
+      if(!$('.inner-box-left').is(':hover')){
+        $('.inner-box-left').fadeOut(300);
+      };
+    });
+    $(".box-img-right").mouseenter(function() {
+        $('.inner-box-right').stop().fadeIn(300);
+        $(".inner-box-right").css("display","grid");
+    });
+    
+    $(".box-img-right, .inner-box-right").mouseleave(function() {
+      if(!$('.inner-box-right').is(':hover')){
+        $('.inner-box-right').fadeOut(300);
+      };
+    });
+});
